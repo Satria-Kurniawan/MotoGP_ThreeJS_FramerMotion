c966eedda3f68fdb3803d75b5a2058d0fb7d638a
Satria-Kurniawan <satriakurniawan2001@gmail.com>
2023-05-28 16:23:54 +0800

    f

--- src/components/ModelViewer.jsx
+++ src/components/ModelViewer.jsx
@@ -1,16 +1,67 @@
 /* eslint-disable react/prop-types */
-import { useRef, useEffect, Suspense } from "react";
+import { useRef, useEffect, useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 import CanvasLoader from "./CanvasLoader";
 
 export default function ModelViewer({ modelPath }) {
+  const [isMobile, setIsMobile] = useState(false);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    canvasRef.current.appendChild(renderer.domElement);
+
+    return () => {
+      renderer.dispose();
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleCanvasScroll = (event) => {
+      window.scrollBy({
+        top: event.deltaY, // Menggunakan event.deltaY untuk menentukan perubahan scroll pada sumbu Y
+        behavior: "smooth", // Untuk scroll yang lancar
+      });
+    };
+
+    const canvas = canvasRef.current;
+
+    // Memasang event listener pada scroll di dalam elemen canvas
+    canvas.addEventListener("wheel", handleCanvasScroll);
+
+    // Membersihkan event listener saat komponen dibongkar
+    return () => {
+      canvas.removeEventListener("wheel", handleCanvasScroll);
+    };
+  }, []);
+
   const Model = () => {
     const { scene } = useGLTF(modelPath);
 
     return (
-      <mesh scale={20} position={[0, -2, 0]} rotation={[0, -2.4, 0]}>
+      <mesh
+        scale={isMobile ? 23 : 25}
+        position={[0, -2, 0]}
+        rotation={[0, -2.4, 0]}
+      >
         <primitive object={scene} />
         <ambientLight intensity={0.5} />
         <hemisphereLight intensity={0.3} groundColor="#ffffff" />
@@ -25,19 +76,6 @@ export default function ModelViewer({ modelPath }) {
       </mesh>
     );
   };
-
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    canvasRef.current.appendChild(renderer.domElement);
-
-    return () => {
-      renderer.dispose();
-    };
-  }, []);
 
   return (
     <Canvas ref={canvasRef}>
