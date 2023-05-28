7c7614d70aa80b83d0ae9715267a5f348ed17335
Satria-Kurniawan <satriakurniawan2001@gmail.com>
2023-05-28 19:22:29 +0800

    f

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -3,8 +3,8 @@ export default function IconButton({ onClick, icon, disabled }) {
   return (
     <button
       onClick={onClick}
-      className={`text-white rounded-full p-5 border border-white hover:bg-purple-500 ${
-        disabled && "opacity-20"
+      className={`text-white rounded-full p-5 border border-white  ${
+        disabled ? "opacity-20 pointer-events-none" : "hover:bg-purple-500"
       }`}
     >
       {icon}
